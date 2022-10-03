737c586a951364c18c40eb2e9ad33245d1948e4c
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-03 20:59:59 +0900

    Add onClick func

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 
 export default function App() {
+  const onClickLog = () => {
+    console.log("Test!");
+  };
   return (
     <ChakraProvider>
-      <Button colorScheme="teal">BBuuttttoonn</Button>
+      <Button colorScheme="teal" onClick={onClickLog}>
+        BBuuttttoonn
+      </Button>
     </ChakraProvider>
   );
 }
