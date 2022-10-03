6c01b19398029f80d5cb8ff29d792c466d46c775
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-04 08:51:40 +0900

    Make menu button by Drawer

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,40 +1,67 @@
-import { Flex, Heading, Link, Box, IconButton } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Link,
+  Box,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  Button,
+  useDisclosure
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          User Management App
-        </Heading>
-      </Flex>
+    <>
       <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
         align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
       >
-        <Box pr={4}>
-          <Link>User List</Link>
-        </Box>
-        <Link>Setting</Link>
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            User Management App
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>User List</Link>
+          </Box>
+          <Link>Setting</Link>
+        </Flex>
+        <IconButton
+          aria-label="menuButton"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        />
       </Flex>
-      <IconButton
-        aria-label="menuButton"
-        icon={<HamburgerIcon />}
-        size="sm"
-        variant="unstyled"
-        display={{ base: "block", md: "none" }}
-      />
-    </Flex>
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">Top</Button>
+              <Button w="100%">User List</Button>
+              <Button w="100%">Setting</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 });
