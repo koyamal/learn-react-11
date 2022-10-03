9f43d4731139647c3d8c7d88a2de612f7e3b6a12
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-04 07:22:30 +0900

    Add global style by extendTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   const onClickLog = () => {
     console.log("Test!");
   };
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal" onClick={onClickLog}>
         BBuuttttoonn
       </Button>
