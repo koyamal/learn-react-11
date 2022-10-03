0eeb82fe757f297c92110aebf214aa3ed16187da
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-04 08:15:03 +0900

    Make home, setting, userManagement pages

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
+import { homeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
   return (
@@ -9,6 +10,26 @@ export const Router: VFC = memo(() => {
       <Route exact path="/">
         <Login />
       </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => {
+          return (
+            <Switch>
+              {homeRoutes.map((route) => {
+                return (
+                  <Route
+                    key={route.path}
+                    exact={route.exact}
+                    path={`${url}${route.path}`}
+                  >
+                    {route.children}
+                  </Route>
+                );
+              })}
+            </Switch>
+          );
+        }}
+      />
     </Switch>
   );
 });
