11c1d90ca2aa0e84d6e9b3eb035d41d61c9ded7f
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-04 08:19:48 +0900

    Add 404 page to root part

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
@@ -30,6 +31,9 @@ export const Router: VFC = memo(() => {
           );
         }}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 });
