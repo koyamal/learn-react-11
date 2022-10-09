00c66d5e634e22c872b3b1fcea67254489cc4911
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-09 17:34:00 +0900

    Make show message at Login

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
   const login = useCallback(
@@ -15,6 +17,7 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "Login is Successful", status: "success" });
             history.push("/home");
           } else {
             alert("Not Found User");
