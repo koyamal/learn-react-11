ce59bf5df424a959ec70d30ef9235ed7c5503b4f
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-09 22:38:47 +0900

    Divide UserCard to a component

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,35 +1,17 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="profilPicture"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              Tom
-            </Text>
-            <Text fontSize="sm" color="gray">
-              Tomoyuki Takahashi
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="Tom"
+          fullName="Tomoyuki Takahashi"
+        />
       </WrapItem>
     </Wrap>
   );
