b5205348f44035ae5c40d8fc8d5d1dfe528fdcec
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-09 11:29:19 +0900

    Add style to user management page

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,5 @@
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>UserManagement page.</p>;
+  return <p style={{ fontSize: "24px" }}>UserManagement page.</p>;
 });
