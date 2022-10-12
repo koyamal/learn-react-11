23a1ee63e72f9f95d017c957ff059a146f83c4b8
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-12 21:22:34 +0900

    Edit Router page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -34,10 +34,10 @@ export const Router: VFC = memo(() => {
             );
           }}
         />
-        <Route path="*">
-          <Page404 />
-        </Route>
       </LoginUserProvider>
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 });
