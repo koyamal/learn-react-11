86381f66e79a51d45172a297d13bf8d00db5efcc
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-12 21:27:12 +0900

    Add useLoginUser to UserManagement

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -13,11 +13,14 @@ import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { useSelectUser } from "../../hooks/useSelectUser";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => {
     getUsers();
