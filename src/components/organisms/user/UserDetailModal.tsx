cb22ce03d1bd548647a266d2555ee52f93bf1d0d
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-13 19:18:14 +0900

    Make Edit input content by admin user

--- src/components/organisms/user/UserDetailModal.tsx
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -10,7 +10,7 @@ import {
   ModalOverlay,
   Stack
 } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 
 import { User } from "../../../types/api/user";
 
@@ -24,6 +24,26 @@ type Props = {
 export const UserDetailModal: VFC<Props> = memo((props) => {
   const { user, isOpen, isAdmin, onClose } = props;
   const isReadOnly = isAdmin === true ? false : true;
+  const [userName, setUserName] = useState<string>(user?.username || "a");
+  const [userFullName, setUserFullName] = useState<string>(user?.name || "a");
+  const [userEmail, setUserEmail] = useState<string>(user?.email || "a");
+  const [userPhone, setUserPhone] = useState<string>(user?.email || "a");
+
+  useEffect(() => {
+    setUserName(user?.username || "");
+    setUserFullName(user?.name || "");
+    setUserEmail(user?.email || "");
+    setUserPhone(user?.phone || "");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user?.username, user?.name, user?.email, user?.phone]);
+
+  const onChangeUserInfo = (
+    e: ChangeEvent<HTMLInputElement>,
+    func: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    func(e.target.value);
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -39,19 +59,35 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
             <Stack spacing={4}>
               <FormControl>
                 <FormLabel>Name</FormLabel>
-                <Input value={user?.username} isReadOnly={isReadOnly} />
+                <Input
+                  value={userName}
+                  isReadOnly={isReadOnly}
+                  onChange={(e) => onChangeUserInfo(e, setUserName)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>Full Name</FormLabel>
-                <Input value={user?.name} isReadOnly={isReadOnly} />
+                <Input
+                  value={userFullName}
+                  isReadOnly={isReadOnly}
+                  onChange={(e) => onChangeUserInfo(e, setUserFullName)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>MAIL</FormLabel>
-                <Input value={user?.email} isReadOnly={isReadOnly} />
+                <Input
+                  value={userEmail}
+                  isReadOnly={isReadOnly}
+                  onChange={(e) => onChangeUserInfo(e, setUserEmail)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>TEL</FormLabel>
-                <Input value={user?.phone} isReadOnly={isReadOnly} />
+                <Input
+                  value={userPhone}
+                  isReadOnly={isReadOnly}
+                  onChange={(e) => onChangeUserInfo(e, setUserPhone)}
+                />
               </FormControl>
             </Stack>
           </ModalBody>
